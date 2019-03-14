67f31c966188b5292fdfe72dca68cb3d8625840a
Soumya-Joy <43504113+Soumya-Joy@users.noreply.github.com>
2019-03-14 16:48:16 +0530

    Combined front-end and back-end

--- signUp.php
+++ signUp.php
@@ -1,5 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php include('backend.php'); ?>
+<html>
 <head>
   <title>SIGN UP</title>
   <meta charset="utf-8">
@@ -13,8 +13,6 @@
          background-color: black;
          text-
         }
-
-
         .formfield *
         {
             vertical-align: middle;
@@ -24,58 +22,51 @@
       color: brown;
   }
 </style>
-<p class=" container-fluid">
-<?php include('backend.php'); ?>
-   <body align="middle">
+<p class=" container-fluid"></p></head>
+	<body  align="middle">
 		<form method="post">
+			<?php include('error.php'); ?>
 			<h1 class="text-danger"><marquee>
                *** WELCOME TO E-POLICE STATION CRIME MANAGEMENT SYSTEM ***
             </marquee>
 				
-			 </h1><br>
+			</h1><br>
             <h2 class="text-info" align="middle" ><u><strong>SIGN UP</strong> </u></h2>
 
             <p align="middle">
             <h3 class ="text-info"> 
-                    
-NAME:
-<strong>*</strong>
-<!-- address -->
+      
+			NAME:
+			<strong>*</strong>
+			<!-- address -->
 
- <input type="text" name="name" required>
-  <br><br>
-            <p class="formfield">
+			<input type="text" name="name" required>
+			<br><br>
+			<p class="formfield">
                 <label for="Textarea">ADDRESS:
                     <strong>*</strong>
                     </label>
-            <textarea id="textarea" rows="3"></textarea>
+            <textarea name="address" id="textarea" rows="3" required></textarea>
             </p>
             
-            
-                <!-- Address: <Textarea name="address" rows="2" cols="40" required></Textarea><br><br> -->
-                
-                AGE: <strong>*</strong>
-             <input type="number" name="age" required min=0 max=110><br><br>
-            
-            
-            GENDER:<strong>*</strong>
+			AGE: <strong>*</strong>
+            <input type="number" name="age" required min=0 max=110><br><br>
+			GENDER:<strong>*</strong>
             <select name="gender" required>
                     <option value = "male">Male</option>
                     <option value = "female">Female</option>
                     <option value = "other">other</option> 
                     </select> <br><br>
-               
+            
             E-MAIL ID: <strong>*</strong> <input type="email" name="email" required><br><br>
-           
-            PHONE NO.:<strong>*</strong> <input type="tel" name="phone" pattern="[0-9]{10}" required><br><br>
-            
-
-			PASSWORD:<strong>*</strong> <input type="password" name="password" id="password" onchange='check_pass();'><br><br>
-            CONFIRM PASSWORD :<strong>*</strong> <input type="password" name="con_password" id="con_password" onchange='check_pass();'><br><br>
-            <span id='message'></span>
-            
-			<input type="Submit" value="SignUp" name="signup" id="submit" disabled >
-            </p>
+          	PASSWORD:<strong>*</strong> 
+          	<input type="password" name="password" id="password" onchange='check_pass();'><br><br>
+			CONFIRM PASSWORD :<strong>*</strong> 
+			<input type="password" name="con_password" id="con_password" onchange='check_pass();'><br><br>
+			PHONE NO.:<strong>*</strong> <input type="tel" name="phone" pattern="[0-9]{10}" required><br><br>
+            <input type="Submit" value="SignUp" name="signup" id="submit" disabled >
+			<span id='Passmessage'></span><br><br>
+		</h3></p>
 		</form>
 		<script>
 			function check_pass() 
@@ -83,18 +74,16 @@ NAME:
 				if(document.getElementById('password').value == document.getElementById('con_password').value)
 				{
 					document.getElementById('submit').disabled = false;
-					document.getElementById('message').style.color = 'green'
-					document.getElementById('message').innerHTML = 'Passwords match';
+					document.getElementById('Passmessage').style.color = 'green'
+					document.getElementById('Passmessage').innerHTML = 'Passwords match';
 				}
 				else
 				{
 					document.getElementById('submit').disabled = true;
-					document.getElementById('message').style.color = 'red'
-					document.getElementById('message').innerHTML = 'Passwords don\'t match';
+					document.getElementById('Passmessage').style.color = 'red'
+					document.getElementById('Passmessage').innerHTML = 'Passwords don\'t match';
 				}
 			}
-        </script>
-        </p>
+		</script>
 	</body>
-
 </html>
