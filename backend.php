3c3a5e33f2d1de96342b52371481c3f2c1eae6c3
pmr123 <40288667+pmr123@users.noreply.github.com>
2019-03-14 16:03:23 +0530

    backend files. Will need some more work

--- backend.php
+++ backend.php
@@ -1,9 +1,11 @@
 <?php
-	//$db = mysqli_connect('localhost','root','','userdata');//connect to database
+    session_start();
+	$errors = array();
+	$db = mysqli_connect('localhost','root','','epol');//connect to database
 	
 	if(isset($_POST['signup']))//if sign up button is clicked
 	{
-	    $name = $_POST['name'];
+		$name = $_POST['name'];
 		$address = $_POST['address'];
 		$age = $_POST['age'];
 		$gender = $_POST['gender'];
@@ -11,13 +13,54 @@
 		$phone = $_POST['phone'];
 		$p1 = $_POST['password'];
 		
-		echo $name." " ;
-		echo $address." ";
-		echo $age." ";
-		echo $gender." ";
-		echo $email." ";
-		echo $phone." ";
-		echo $p1." ";
+		$query = "SELECT * FROM userdata WHERE email = '$email'"; 
+		$result = mysqli_query($db,$query);
+		$num_rows = mysqli_num_rows($result);
+		if($num_rows!=0)
+		{
+			array_push($errors,"The Email has an account assosiated with it");
+		}
+		else
+		{
+			$password = md5($p1);//Encrypt password
+			$sql = "INSERT INTO userdata (name,address,age,gender,email,phone,pass,userType) VALUES('$name','$address','$age','$gender','$email','$phone','$password','c')";
+			mysqli_query($db,$sql);
+			$_SESSION['username'] = $email;
+			$_SESSION['success'] = "You are now logged in";
+			$_SESSION['type'] = 'c';
+			header('location: demoHome.php');// redirecting to homepage
+		}
 		
 	}	
+	
+	if(isset($_POST['signin']))//if sign in button is clicked 
+	{
+		$email = $_POST['email'];
+		$p1 = $_POST['password'];
+		$password = md5($p1);//encypt before comparing
+		$query = "SELECT * FROM userdata WHERE email = '$email' AND pass='$password'"; 
+		$result = mysqli_query($db,$query);
+		$num_rows = mysqli_num_rows($result);
+		$data = mysqli_fetch_assoc($result);
+		if($num_rows!=0)
+		{	
+			echo "logged in";
+			$_SESSION['success'] = "You are now logged in";
+			$_SESSION['type'] = $data['userType'];
+			echo "lol";
+			//header('location: demoHome.php');// redirecting to homepage
+		}
+		else
+		{
+			array_push($errors,"Wrong username/password combination");
+		}
+	}
+	//logout
+	if(isset($_GET['logout']))
+	{
+		session_destroy();
+		unset($_SESSION['username']);
+		header('location:signIn.php');	
+			
+	}
 ?>
